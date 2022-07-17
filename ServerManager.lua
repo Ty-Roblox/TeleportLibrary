1e0876b217222ce2a98e85add06eb415dade988f
Ty <83230206+Ty-Roblox@users.noreply.github.com>
2022-07-17 16:10:46 -0700

    Update ServerManager.lua

--- ServerManager.lua
+++ ServerManager.lua
@@ -2,6 +2,34 @@ local HttpService = game:GetService("HttpService")
 local ServerManager = {}
 ServerManager.__index = ServerManager
 
+local function Print(...)
+    local Str = ''
+    local Idx = 0
+    for i,v in pairs({...}) do
+        if Idx == 0 then
+            Str = string.format('%s', tostring(v))
+        else
+
+            Str = string.format('%s   %s', Str, tostring(v))
+        end
+        Idx = Idx + 1
+    end
+    rconsoleprint(string.format('%s\n', Str))
+end
+
+local function DumpTable(o)
+   if type(o) == 'table' then
+      local s = '{ '
+      for k,v in pairs(o) do
+         if type(k) ~= 'number' then k = '"'..k..'"' end
+         s = s .. '['..k..'] = ' .. DumpTable(v) .. ','
+      end
+      return s .. '} '
+   else
+      return tostring(o)
+   end
+end
+
 function ServerManager.new(Options)
     if not Options then
         Options = {}
@@ -16,12 +44,23 @@ end
 
 function ServerManager:GetServerPage(Cursor)
     local Request=syn.request({
-        Url=string.format('https://games.roblox.com/v1/games/%s/servers/Public?sortOrder=%s&limit=100%s', tostring(self.Settings.PlaceId), self.Settings.Method, (Cursor and string.format('cursor=%s', Cursor)) or '');
-        Method='GET';
+        Url = string.format('https://games.roblox.com/v1/games/%d/servers/Public?sortOrder=%s&limit=100%s', self.Settings.PlaceId, 'Desc', (Cursor and string.format('cursor=%s', Cursor)) or '');
+        Method = 'GET';
     })
-    if Request and Request.Success then
-        return HttpService:JSONDecode(Request.Body)
+    if self.Settings.Debug then
+        Print('DEBUG GETSERVERPAGE', string.format('https://games.roblox.com/v1/games/%d/servers/Public?sortOrder=%s&limit=100%s', self.Settings.PlaceId, self.Settings.Method, (Cursor and string.format('cursor=%s', Cursor)) or ''))
     end
+    if Request and Request.Success then
+        if self.Settings.Debug then
+            Print("DEBUG GetServerPage", #HttpService:JSONDecode(Request.Body).data)
+        end
+        return HttpService:JSONDecode(Request.Body)
+    else
+        if self.Settings.Debug then
+            Print("DEBUG GetServerPage", "REQUEST FAILED 1")
+        end
+    end
+    Print("DEBUG GetServerPage", "REQUEST FAILED 2")
 end
 
 function ServerManager:GetAllServers()
@@ -30,6 +69,10 @@ function ServerManager:GetAllServers()
     local Idx = 0
     while true do
         local Page = self:GetServerPage(Cursor)
+        if self.Settings.Debug then
+            Print("DEBUG Page.data", self.Settings.PlaceId, self.Settings.Method, Idx, ":", #Page.data)
+            Print(DumpTable(Page))
+        end
         if Page and Page.data then
             Idx += 1
             for i,v in ipairs(Page.data) do
